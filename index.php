b00140c93c08780d3da214e61b9af2aaa17ed11d
jasonalex <info@jasonalex.net>
2015-05-29 18:10:27 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	require 'load/header.php';
+?>
+
+<h1>Hello World 2015</h1>
+<div class="biglinks">
+	<a href="#">Nominations</a>
+	<a href="#">Competition Rules</a>
+	<a href="#">Sponsors</a>
+	<a href="#">Where Is It?</a>
+	<a href="#">Contact Us</a>
+</div>
+<p>Body Text.</p>
+
+<?php
+	require 'load/footer.php';
+?>
---
+++ load/header.php
@@ -0,0 +1,11 @@
+<?php
+	$ssyear = "2015";
+?>
+<html>
+<head>
+	<title><?php echo "Saratoga Spectacular $ssyear"; ?></title>
+</head>
+<link rel="stylesheet" type="text/css" href="load/style.css">
+<body>
+<a class="logo" id="ss-cornerlogo" href="/"><?php echo "Saratoga Spectacular $ssyear"; ?></a>
+<a class="logo" id="mrfsa-cornerlogo" href="http://mrfsa.org.au">Presented by MRFSA</a>
